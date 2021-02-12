ac5e127fe2abb2957546b0cd7c8e57386a3c0eef
DESKTOP-02JARBC\Admin <semsem6661@gmail.com>
2021-02-12 04:40:33 +0200

    initial commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use Illuminate\Support\Carbon;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return Item::orderBy('created_at', 'DESC')->get();
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newItem       = new Item();
+
+        // $newItem->name = $request['name'];
+        $newItem->name = $request->item['name'];
+
+        $newItem->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $existingItem = Item::find($id);
+
+        if ($existingItem) {
+
+            $existingItem->completed    = $request->item['completed'] ? true : false;
+
+            $existingItem->completed_at = $request->item['completed'] ? Carbon::now() : false;
+            
+            $existingItem->save();
+
+            return $existingItem;
+
+        } else {
+
+            return "Item is not found!";
+
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $existingItem = Item::find($id);
+
+        if ($existingItem) {
+
+            $existingItem->delete();
+
+            return "Item deleted successfully";
+
+        } else {
+
+            return "Item not found";
+        }
+    }
+}
---
+++ resources/js/vue/listItem.vue
@@ -0,0 +1,102 @@
+<template>
+ 
+    <div class="item">
+        <input 
+             type="checkbox"
+             @change="updateCheck()"
+             v-model="item.completed"
+        />
+        <span :class="[item.completed ? 'completed' : '', 'itemText']"> {{ item.name }} </span>
+        <button @click="removeItem()" class="trashcan" >
+            <font-awesome-icon icon="trash" />
+        </button>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['item'],
+    methods: {
+
+        updateCheck() {
+
+          axios.put('api/item/' + this.item.id, {
+
+             item: this.item
+
+          } )
+
+          .then(responce => {
+
+             if (responce.status == 200) {
+
+                 this.$emit('itemChanged');
+             }
+
+          })
+
+          .catch ( error => {
+
+              console.log(error);
+
+          })
+
+        },
+
+        removeItem() {
+
+            axios.delete('api/item/' + this.item.id)
+            .then(responce => {
+
+                if (responce.status == 200) {
+
+                    this.$emit('itemChanged');
+                }
+            })
+
+            .catch (error => {
+
+                console.log(error);
+                
+            })
+        }
+
+    }
+    
+}
+</script>
+
+<style scoped>
+
+    .completed {
+
+        text-decoration: line-through;
+        color: #999999;
+
+    }
+
+    .itemText {
+
+        width: 100%;
+        margin-left: 20px;
+    }
+
+    .item {
+
+       display: flex;
+       justify-content: center;
+       align-items: center;
+
+    }
+
+    .trashcan {
+
+        background: #e6e6e6;
+        border: none;
+        color: #555353;
+        outline: none;
+    }
+
+</style>
